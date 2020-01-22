aaa2fda00ea6f4134eb06fc17fba93e682bdb11d
Austin Rhoads <austinrrhoads@ymail.com>
2020-01-22 22:44:23 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board(ary)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+def display_board(ary=[" "," "," "," "," "," "," "," "," "])
+  puts " #{ary[0]} | #{ary[1]} | #{ary[2]} "
   puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts " #{ary[3]} | #{ary[4]} | #{ary[5]} "
   puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts " #{ary[6]} | #{ary[7]} | #{ary[8]} "
 end
 board = [" "," "," "," "," "," "," "," "," "]
+display_board()
